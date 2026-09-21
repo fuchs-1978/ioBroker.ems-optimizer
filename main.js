613dc6dadd0331b97f9ec5d3df1751aedfc38cf2
fuchs-1978 <61315711+fuchs-1978@users.noreply.github.com>
2026-09-21 15:18:51 +0200

    Release 0.17.0-alpha.14: update main.js

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ class EmsOptimizer extends utils.Adapter {
         this.publishMappingStatus();
         await this.wallboxOutput.initialize();
         await this.setStateAsync("info.connection", true, true);
-        this.log.info("EMS Optimizer 0.17.0-alpha.13 started; alpha outputs require explicit release");
+        this.log.info("EMS Optimizer 0.17.0-alpha.14 started; alpha outputs require explicit release");
     }
 
     async preloadStates() {
@@ -206,6 +206,7 @@ class EmsOptimizer extends utils.Adapter {
             WallboxStartReserve_W: ["wallboxStartReserveW", 300],
             WallboxStartDelay_s: ["wallboxStartDelayS", 30],
             WallboxMinimumRunTime_s: ["wallboxMinimumRunTimeS", 120],
+            WallboxStopDelay_s: ["wallboxStopDelayS", 120],
             WallboxRestartHandoffSettle_s: ["wallboxRestartHandoffSettleS", 10],
             WallboxMeasurementMaxAge_s: ["wallboxMeasurementMaxAgeS", 30],
             PhaseSwitchTransition_s: ["phaseSwitchTransitionS", 90],
